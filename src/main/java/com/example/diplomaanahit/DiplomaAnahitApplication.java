e75aaa84e5e8b20f5e4e890ff61188f1e3634187
Anita889 <manahit889@gmail.com>
2024-11-21 21:05:32 +0400

    first commit

---
+++ src/main/java/com/example/diplomaanahit/DiplomaAnahitApplication.java
@@ -0,0 +1,13 @@
+package com.example.diplomaanahit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiplomaAnahitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiplomaAnahitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/diplomaanahit/DiplomaAnahitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.diplomaanahit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiplomaAnahitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
