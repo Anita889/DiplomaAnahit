ad399301ca0c372fb6f4c8179107b22c2bed00cd
Anita889 <manahit889@gmail.com>
2024-11-22 17:14:18 +0400

    Dockerize all backend project

---
+++ shells/deploy-DockerFile.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+export GRADLE_OPTS="-Xmx64m -Dorg.gradle.jvmargs='-Xmx1g'"
+export DEFAULT_JVM_OPTS="-Xmx256m"
+
+cd /DiplomaAnahit
+
+./gradlew clean
+
+./gradlew build
+
+java -jar ./build/libs/DiplomaAnahit.jar
